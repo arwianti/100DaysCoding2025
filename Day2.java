8aaffd00d4419f626d439b96f827db4c6fbca1b1
arwianti <arwiarwianti@gmail.com>
2025-09-09 20:29:28 +0800

    Create Day2.java

---
+++ Day2.java
@@ -0,0 +1,17 @@
+public class main {
+public static void main (String[] args) {
+  String nama = "arwianti" ;
+  int umur = 18 ;
+  String prodi = "informatika" ;
+
+
+  System.out.print("halo ") ;
+  System.out.print("semua ") ;
+  System.out.print(" nama\t: %s\n umur\t: %d tahun\n Prodi\t: % s\n" , nama, umur, prodi) ;
+    }
+}
+    
+
+
+  
+  
