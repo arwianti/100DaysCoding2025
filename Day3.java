1a967107068c8eb40eefbc45b158bf02fdcbdd58
arwianti <arwiarwianti@gmail.com>
2025-09-10 20:50:41 +0800

    Create Day3.java

---
+++ Day3.java
@@ -0,0 +1,23 @@
+package day3;
+public class day3{
+
+
+  public statik void main (string[] args) {
+    system.out.println("arwianti") ;
+    //2 jenis komentar yaitu:
+    /*komentar satu baris dan
+      *komentar multi baris
+      */
+    //komentar satu baris diawali dengan dua garis miring dan berlaku hingga akhir baris. 
+    //ini adalah komentat satu baris
+
+    
+    /*komentar multi baris: diawali dan diakhiri dengan garis miring dan bintang. 
+      /*ini adalah komentar multi-baris
+      *dan seperti ini jika dianter
+      */
+    //
+  }
+}
+  
+  
