d14c82ad8840ebf318284c4c0e9ac6e3993f1fb6
arwianti <arwiarwianti@gmail.com>
2025-09-11 20:25:22 +0800

    Create Day4.java

---
+++ Day4.java
@@ -0,0 +1,16 @@
+public class day4 {
+static void main(string[] args) {
+    
+ System.out.println("Hello\nWorld!") ;
+     // tab (/t) tab (spasi panjang) 
+ System.out.println("nama\tArwianti") ;
+     //Backslash (\\) (yaitu menampilkan karakter Backlash) 
+ System.out.println("C:\\users\\arwi\\"); // \" menampilkan tanda kutip ganda 
+ System.out.println("saya : \"kuliah\"") ;   // \"
+ System.out.println("hari : \'jumat\'") ;  // '
+ System.out.println("hai\b") ; // \b menghapus 1 karakter sebelumnya
+ System.out.println("hai\rHi!") ; // \r mengembalikan kursor ke awal baris 
+  
+  
+
+  
