c6297fe406c5501fdc61145d1df5dafed5fa1cf9
arwianti <arwiarwianti@gmail.com>
2025-09-14 20:57:21 +0800

    Create Day7.java

---
+++ Day7.java
@@ -0,0 +1,15 @@
+public class Day7{
+
+  
+    public static void main(String[] args) {
+        char inisialNama = 'A';
+        boolean javasusah = true;
+
+
+
+      
+        System.out.println("inisial nama saya : " + inisialNama);
+        System.out.println("Java itu susah : " + javasusah);
+      
+    }  
+  }
