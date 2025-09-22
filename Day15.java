c201da40d40151b8d13707387dce1f1a3368e883
arwianti <arwiarwianti@gmail.com>
2025-09-22 20:54:33 +0800

    Create Day15.java

---
+++ Day15.java
@@ -0,0 +1,15 @@
+public class main{
+  public static void main(String[] args) {
+
+     int a = 50;
+     int b = 20;
+
+  //penjumlahan
+  int hasilpenjumlahan = a + b ;
+
+  // pengurangan
+  int hasilpengurangan = a - b ;
+
+
+    System.out.println("hasil penjumlahan: " + hasilpenjumlahan + ( "\nhasil pengurangan: " + hasilpengurangan)) ;
+    
