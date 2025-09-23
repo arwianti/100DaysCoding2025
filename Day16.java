19076b196ad76a3f0a78d1bf046375e022060cd6
arwianti <arwiarwianti@gmail.com>
2025-09-23 21:48:00 +0800

    Create Day16.java

---
+++ Day16.java
@@ -0,0 +1,17 @@
+public class Day16.java{
+  public static void main(String[] args) {
+
+     int a = 20;
+     int b = 12;
+
+  //perkalian
+  int a = a * b ;
+
+  // pembagian
+  int b = a \ b ;
+
+
+    System.out.println("hasil perkalian: " + a + ( "\nhasil pembagian: " + b)) ;
+    
+  }
+}
