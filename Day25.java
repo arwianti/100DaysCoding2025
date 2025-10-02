7ba884659117109b4a809e9121ab065e070aec54
arwianti <arwiarwianti@gmail.com>
2025-10-02 22:59:28 +0800

    Create Day25.java

---
+++ Day25.java
@@ -0,0 +1,22 @@
+package Day25;
+
+
+public class java {
+    public static void main(String[] args) {
+        int angka = 5;
+        
+               
+        angka++; //ditambah 1
+        System.out.println("setelah increment: " + angka);
+        
+         angka--;//dikurang 1 
+        System.out.println("setelah decrement: " + angka);
+        
+
+
+        
+       
+        
+    }
+    
+}
