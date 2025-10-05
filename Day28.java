34dabb9073ca2a84e207e2093c36dc01a5eeb2f2
arwianti <arwiarwianti@gmail.com>
2025-10-05 20:37:57 +0800

    Create Day28.java

---
+++ Day28.java
@@ -0,0 +1,19 @@
+package Day28;
+
+
+public class java {
+    public static void main(String[] args) {
+        
+        int a=7;
+        int b=9;
+        
+      
+        //operator ==
+        System.out.println("apakah a == b: " + (a==b));
+          
+        //operator !=
+        System.out.println("apakah a != b: " + (a!=b));      
+        
+        
+    }   
+}
