11302a251e699069e0d0c91af8d67710fc8d10e0
arwianti <arwiarwianti@gmail.com>
2025-10-08 22:38:39 +0800

    Create Day31.java

---
+++ Day31.java
@@ -0,0 +1,18 @@
+package Day31.java;
+
+
+public class Day31Java {
+
+    
+    public static void main(String[] args) {
+       int umur = 18;
+       boolean ktp =true;
+       
+       if (umur >= 17 && ktp ){
+           System.out.println("anda sudah bisa membuat ktp");
+       }else{
+           System.out.println("anda belum bisa membuat ktp");
+       }
+    }
+    
+}
