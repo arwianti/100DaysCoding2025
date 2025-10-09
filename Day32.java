785555164933ed216d5ab8bcbb165c2d5d3238bc
arwianti <arwiarwianti@gmail.com>
2025-10-09 20:31:33 +0800

    Create Day32.java

---
+++ Day32.java
@@ -0,0 +1,15 @@
+package Day23;
+
+public class java{
+ 
+    public static void main(String[] args) {
+        int nilai = 70;
+        int absen = 85;
+
+        if (nilai >= 75 || absen >= 80) {
+            System.out.println("Lulus");
+        } else {
+            System.out.println("Tidak Lulus");
+        }
+    }
+} 
