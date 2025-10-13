d3b54867828e4818b8cfca50ee406b8dd38c6bab
arwianti <arwiarwianti@gmail.com>
2025-10-13 21:48:45 +0800

    Create Day36.java

---
+++ Day36.java
@@ -0,0 +1,15 @@
+
+package Day36;
+public class java {
+    public static void main(String[] args) {
+        int nilai = 80;
+
+        if (nilai >= 90) {
+            System.out.println("Nilai kamu A - Sangat baik!");
+        } else if (nilai >= 75) {
+            System.out.println("Nilai kamu B - Lulus!");
+        } else {
+            System.out.println("Nilai kamu C - Tidak lulus!");
+        }
+    }
+}
