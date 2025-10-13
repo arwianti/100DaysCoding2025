ae34c3ceca88b7132c959bd24373948768b7a57a
arwianti <arwiarwianti@gmail.com>
2025-10-13 23:05:26 +0800

    Update Day36.java

--- Day36.java
+++ Day36.java
@@ -1,15 +1,19 @@
 
 package Day36;
+import java.util.Scanner;
 public class java {
     public static void main(String[] args) {
-        int nilai = 80;
-
-        if (nilai >= 90) {
-            System.out.println("Nilai kamu A - Sangat baik!");
-        } else if (nilai >= 75) {
-            System.out.println("Nilai kamu B - Lulus!");
-        } else {
-            System.out.println("Nilai kamu C - Tidak lulus!");
+       Scanner input= new Scanner(System.in);
+        System.out.print("masukkan nilai kamu: ") ;
+        int nilai =input.nextIn() ;
+        
+        if (nilai >=80) {
+        if (nilai >=90) {
+            System.out.println("Nilai kamu A  Sangat baik");
+        } else  {
+            System.out.println("Nilai kamu B Lulus");
+        } else if (nilai >=75) {
+            System.out.println("Nilai kamu C Tidak lulus!");
         }
     }
 }
