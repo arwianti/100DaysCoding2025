de2ca41428789f4b67532b05b3c71445f27895bb
arwianti <arwiarwianti@gmail.com>
2025-10-25 23:22:40 +0800

    Create Day48.java

---
+++ Day48.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class java{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan angka pertama: ");
+        double angka1 = input.nextDouble();
+
+        System.out.print("Masukkan operator (+, -, *, /, %): ");
+        char operator = input.next().charAt(0);
+
+        System.out.print("Masukkan angka kedua: ");
+        double angka2 = input.nextDouble();
+
+        double hasil;
+
+        switch (operator) {
+            case '+':
+                hasil = angka1 + angka2;
+                break;
+            case '-':
+                hasil = angka1 - angka2;
+                break;
+            case '*':
+                hasil = angka1 * angka2;
+                break;
+            case '/':
+                hasil = angka1 / angka2;
+                break;
+            case '%':
+                hasil = angka1 % angka2;
+                break;
+            default:
+                System.out.println("Operator tidak valid!");
+                return;
+        }
+
+        System.out.println("Hasil: " + hasil);
+        input.close();
+    }
+}
