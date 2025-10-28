d88d54b8ce60878a4d00b854efc7cec03a2643a7
arwianti <arwiarwianti@gmail.com>
2025-10-28 21:46:49 +0800

    Create Day51.java

---
+++ Day51.java
@@ -0,0 +1,12 @@
+package day51;
+
+public class java {
+    public static void main(String[] args) {
+
+        
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Angka ke-" + i);
+        }
+
+    }
+}
