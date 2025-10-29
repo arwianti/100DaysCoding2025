ae70fbd435dd9bc55845c09d793cf5edecb7589f
arwianti <arwiarwianti@gmail.com>
2025-10-29 19:45:24 +0800

    Create Day52.java

---
+++ Day52.java
@@ -0,0 +1,11 @@
+package Day52;
+public class java {
+    public static void main(String[] args) {
+        int i = 1; // nilai awal
+
+        while (i <= 20) { 
+            System.out.print(i);
+            
+        }
+    }
+}
