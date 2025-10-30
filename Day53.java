439404ac1fb4efe5b0cb57c177644773d7bded0c
arwianti <arwiarwianti@gmail.com>
2025-10-30 21:59:06 +0800

    Create Day53.java

---
+++ Day53.java
@@ -0,0 +1,15 @@
+public class BreakSederhana {
+    public static void main(String[] args) {
+        
+        
+       for (int i = 1; i <= 5; i++) {
+            
+            
+        if (i == 3) {
+               break; 
+            }
+         System.out.println("Angka: " + i);
+        }
+        System.out.println("Perulangan berhenti di angka 3");
+    }
+}
